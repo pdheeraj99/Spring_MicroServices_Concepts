0e667a7f8585c29c2c35c618827b82633e705fdb
pdheeraj99 <pilladheeraj@gmail.com>
2025-05-08 18:22:34 +0530

    Created Spring Mongo project 1

---
+++ Spring_MongoDB_Projects/spring_mongo_connection_example_1/src/main/java/com/learn/spring_mongo_connection_example/SpringMongoConnectionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring_mongo_connection_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoConnectionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoConnectionExampleApplication.class, args);
+	}
+
+}
---
+++ Spring_MongoDB_Projects/spring_mongo_connection_example_1/src/test/java/com/learn/spring_mongo_connection_example/SpringMongoConnectionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring_mongo_connection_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoConnectionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
